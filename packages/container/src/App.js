6c63ef5807f74ceb1428747b4738a2c5110c49f0
kdirschl <info@audioshaft.com>
2020-11-25 11:39:46 -0500

    missing redirect import

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,5 +1,5 @@
 import React, { lazy, Suspense, useState, useEffect } from 'react'
-import { Router, Route, Switch } from 'react-router-dom'
+import { Router, Route, Switch, Redirect } from 'react-router-dom'
 import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles'
 import { createBrowserHistory } from 'history'
 
